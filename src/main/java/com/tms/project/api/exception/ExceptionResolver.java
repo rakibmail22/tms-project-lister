1423aaba74330bd47943ef9a1b06a7247fa55d6f
Bashir Ahmed Rakib <bashir.rakib@labforward.io>
2023-04-30 00:10:31 +0600

    add unauthorized config handling

--- src/main/java/com/tms/project/api/exception/ExceptionResolver.java
+++ src/main/java/com/tms/project/api/exception/ExceptionResolver.java
@@ -1,7 +1,9 @@
 package com.tms.project.api.exception;
 
 import com.tms.project.api.model.response.ErrorResponse;
+import feign.FeignException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -31,6 +33,16 @@ public class ExceptionResolver {
 	@ExceptionHandler(ConfigurationAlreadyExistsException.class)
 	public ErrorResponse handleMultiConfiguration(ConfigurationAlreadyExistsException ex) {
 		return new ErrorResponse(ex.getResourceName(), ex.getMsg());
+	}
+
+	@ExceptionHandler
+	public ResponseEntity<ErrorResponse> handleFeignException(FeignException ex) {
+		String responseMsg = (ex.status() == HttpStatus.UNAUTHORIZED.value()) ?
+		                     "Invalid configuration, Please update configuration with valid username and password" :
+		                     ex.getMessage();
+		ErrorResponse errorResponse = new ErrorResponse(ex.request().url(), responseMsg);
+		return ResponseEntity.status(ex.status()).body(errorResponse);
+
 	}
 
 
